70f14a7ac2ce5890d2b468c06e714eaa495eea4f
eralpkor <eralp.ny@gmail.com>
2019-05-15 21:00:16 -0400

    new post #2

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,10 @@
+// Home page type effect
+// var typed = new Typed('#cover', {
+//   strings: ["Find the love in what you do."],
+//   typeSpeed: 10,
+//   loop: false,
+//   showCursor: false
+// });
 
 
 // Footer current year
